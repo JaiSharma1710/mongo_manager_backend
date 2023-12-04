c25cb4033494620ef8d1f198aa234b2deca7a102
Jai Sharma <jai.sharma@universityliving.com>
2023-12-04 15:11:43 +0530

    fix the code for html form

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const DB = require('./db');
 const cors = require('cors');
 require('dotenv').config();
 const { Parser } = require('@json2csv/plainjs');
-const {
-  uploadFileToAppwrite,
-  downloadFileFromAppwrite,
-} = require('./appwrite');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
 app.post('/getCollections', async (req, res) => {
   const { mongoUri, dbName } = req.body;
@@ -81,10 +78,9 @@ app.post('/runQuery', async (req, res) => {
 });
 
 app.post('/createCsv', async (req, res) => {
-  const {
-    selectedQuery: query,
-    selected_db: { mongoUri, dbName },
-  } = await req.body;
+  const { mongoUri, dbName, query } = req.body;
+
+  console.log(req.body);
 
   try {
     if (!mongoUri) {
@@ -118,26 +114,17 @@ app.post('/createCsv', async (req, res) => {
     const data = await eval(`${finalQuery}.toArray()`);
     const parser = new Parser({ header: true });
     const csvData = parser.parse(data);
-    const uploadResponse = await uploadFileToAppwrite(csvData);
-    const fileId = uploadResponse.$id;
-    res.send(
-      `https://mongo-manager-backend.vercel.app/downloadcsv?fileId=${fileId}`,
-    );
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Disposition', 'attachment;filename=data.csv');
+    res.statusCode = 200; // OK
+    res.end(csvData);
   } catch (error) {
     return res.status(500).json({
       message: `some error occurred while executing ERROR_MESSAGE:${error}`,
     });
   }
-});
-
-app.get('/downloadcsv', async (req, res) => {
-  const { fileId } = req.query;
-  const file = await downloadFileFromAppwrite(fileId);
-  res.setHeader('Content-Type', 'text/csv');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Disposition', 'attatchment;filename=boka.csv');
-  res.statusCode = 200; // OK
-  res.end(file);
 });
 
 app.listen(9000, () => console.log('running on port 9000'));
